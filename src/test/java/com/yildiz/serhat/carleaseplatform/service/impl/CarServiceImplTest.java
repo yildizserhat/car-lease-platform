6580fb5481b631103d2c63d3f2d4bcb6b6ba553c
serhatyildiz <yildiz_serhat@hotmail.com>
2023-03-30 23:19:41 +0200

    Test added.

--- src/test/java/com/yildiz/serhat/carleaseplatform/service/impl/CarServiceImplTest.java
+++ src/test/java/com/yildiz/serhat/carleaseplatform/service/impl/CarServiceImplTest.java
@@ -1,5 +1,9 @@
 package com.yildiz.serhat.carleaseplatform.service.impl;
 
+import com.yildiz.serhat.carleaseplatform.controller.request.CarRequestDTO;
+import com.yildiz.serhat.carleaseplatform.controller.request.LeaseRateRequestDTO;
+import com.yildiz.serhat.carleaseplatform.domain.entity.Car;
+import com.yildiz.serhat.carleaseplatform.domain.entity.Customer;
 import com.yildiz.serhat.carleaseplatform.repository.CarRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,8 +11,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CarServiceImplTest {
@@ -21,9 +32,44 @@ class CarServiceImplTest {
     private CarRepository repository;
 
     @Test
-    public void shouldGetAllItems() {
-   //     orderItemService.getAllOrderItems();
-        verify(repository, atLeastOnce()).findAll();
+    public void shouldCreateCar() {
+        CarRequestDTO carRequestDTO = new CarRequestDTO("make", "BMW", "320i", 4, "co2",
+                new BigDecimal(50000), new BigDecimal(45000), "3", new LeaseRateRequestDTO(new BigDecimal(40000), 10, 4.5));
+
+        when(repository.save(any())).thenReturn(Car.builder().id(3L).build());
+        carService.createCar(carRequestDTO);
+
+        verify(customerService, atLeastOnce()).getCustomerById(3L);
+    }
+
+    @Test
+    public void shouldGetAllCarsByCustomer() {
+        Customer customer = Customer.builder().id(3L).build();
+        when(customerService.getCustomerById(3L)).thenReturn(customer);
+        when(repository.findByCustomer(customer)).thenReturn(List.of(Car.builder().customer(customer).build()));
+
+        List<Car> allCarsByCustomer = carService.getAllCarsByCustomer(3L);
+
+        assertEquals(allCarsByCustomer.get(0).getCustomer().getId(), 3L);
+    }
+
+    @Test
+    public void shouldGetCarById() {
+        when(repository.findById(1L)).thenReturn(Optional.of(Car.builder().id(1L).build()));
+
+        Car carById = carService.getCarById(1L);
+
+        assertEquals(carById.getId(), 1L);
+    }
+
+    @Test
+    public void shouldUpdateCar(){
+
+    }
+
+    @Test
+    public void shouldDeleteCar(){
+
     }
 
 }
--- src/test/java/com/yildiz/serhat/carleaseplatform/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/yildiz/serhat/carleaseplatform/service/impl/CustomerServiceImplTest.java
@@ -1,7 +1,39 @@
 package com.yildiz.serhat.carleaseplatform.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.yildiz.serhat.carleaseplatform.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
+
+    @InjectMocks
+    private CustomerServiceImpl customerService;
+    @Mock
+    private CustomerRepository repository;
+
+    @Test
+    public void shouldCreateCustomer() {
+
+    }
+
+    @Test
+    public void shouldGetCustomerById() {
+
+    }
+
+    @Test
+    public void shouldDeleteCustomer() {
+
+
+    }
+
+    @Test
+    public void shouldUpdateCustomer() {
+
+    }
 
 }
